5a618f6de82bd84710558df68e2e4f4131710ef9
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-06-22 18:24:37 +0100

    initial commit

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo chmod 744 -R /opt/elrond/
-sudo chown sansforensics:sansforensics -R /opt/elrond/
-export PATH=$PATH:/opt/elrond
-sudo chmod +x /opt/elrond/elrond/config.sh
-sudo chmod +x /opt/elrond/elrond/elrond.py
-clear
-cd /opt/elrond/elrond
-sudo ./config.sh
